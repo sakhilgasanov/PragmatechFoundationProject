d7e0ecc58f1f72324ea85a3831fc592b8c938c64
sakhilgasanov <102470995+sakhilgasanov@users.noreply.github.com>
2022-04-19 22:58:00 +0400

    yeni kod

---
+++ taskss.py
@@ -0,0 +1,8 @@
+ad=str(input("istifadeci adinizi daxiledin"))
+parol=str(input("parolunuzu daxil edin"))
+x="istifadeci_adi"
+y="parol"
+if x==ad and y==parol:
+        print(x,y)
+else: 
+        print("istifadeci adi ve ya parol duzgun deyil")
