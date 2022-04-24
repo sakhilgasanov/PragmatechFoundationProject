e677ee612ead13d9d84e976a2e96089167e28314
sakhilgasanov <102470995+sakhilgasanov@users.noreply.github.com>
2022-04-24 12:25:03 +0400

    new code

--- calculator.py
+++ calculator.py
@@ -1,6 +1,14 @@
 a=int(input("a ededini daxil edin: "))
 b=int(input("b ededini daxil edin: "))
-c=int(input("c ededini daxil edin: "))
 emeliyyat=input("emeliyyati daxil edin: ")
 if emeliyyat=="+":
-    print
+    print(a+b)
+elif emeliyyat=="-":
+    print(a-b)
+elif emeliyyat=="*":
+    print(a*b)
+elif emeliyyat=="/":
+    print(a/b)
+else:
+    print("daxil etdiyiniz emeliyyat duzgun deyil")
+    
--- drive.py
+++ drive.py
@@ -1,9 +1,10 @@
-a=int(input("deyeri daxil edin: "))
-if a>0:
-    print("deyer mwsbetdir")
-elif a<0:
-    print("deyer menfidir")
-elif a==0:
-    print("deyer sifirdir")
-else:
+try:
+    a=int(input("deyeri daxil edin: "))
+    if a>0:
+        print("deyer mwsbetdir")
+    elif a<0:
+        print("deyer menfidir")
+    else:
+        print("deyer sifirdir")
+except:
     print("siz eded daxil etmemisiniz")
