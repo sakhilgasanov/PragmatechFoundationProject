78fe807f85b84c7260fef139aeca89d049833cd5
sakhilgasanov <102470995+sakhilgasanov@users.noreply.github.com>
2022-04-24 12:44:20 +0400

    new code

--- calculator.py
+++ calculator.py
@@ -1,14 +1,42 @@
-a=int(input("a ededini daxil edin: "))
-b=int(input("b ededini daxil edin: "))
-emeliyyat=input("emeliyyati daxil edin: ")
-if emeliyyat=="+":
-    print(a+b)
-elif emeliyyat=="-":
-    print(a-b)
-elif emeliyyat=="*":
-    print(a*b)
-elif emeliyyat=="/":
-    print(a/b)
-else:
-    print("daxil etdiyiniz emeliyyat duzgun deyil")
+#try:
+#    a=int(input("a ededini daxil edin: "))
+#    b=int(input("b ededini daxil edin: "))
+#    emeliyyat=input("emeliyyati daxil edin: ")
+#   if emeliyyat=="+":
+#       print(a+b)
+#    elif emeliyyat=="-":
+#         print(a-b)
+#    elif emeliyyat=="*":
+#        print(a*b)
+#    elif emeliyyat=="/":
+#        print(a/b)
+#    else:
+#        print("daxil etdiyiniz emeliyyat duzgun deyil")
+#except:
+#   print("siz eded daxil etmemisiniz")
     
+    
+def Toplama(a,b):
+    print(a+b)
+def Cixma(a,b):
+    print(a-b)
+def Vurma(a,b):
+    print(a*b)
+def Bolme(a,b):
+    print(a/b)
+try:
+    a=int(input("a ededini daxil edin: "))
+    b=int(input("b ededini daxil edin: "))
+    emeliyyat=input("emeliyyati daxil edin: ")
+    if emeliyyat=="+":
+        Toplama(a,b)
+    elif emeliyyat=="-":
+        Cixma(a,b)
+    elif emeliyyat=="*":
+        Vurma(a,b)
+    elif emeliyyat=="/":
+        Bolme(a,b)
+    else:
+        print("daxil etdiyiniz emeliyyat duzgun deyil")
+except:
+    print("siz eded daxil etmemisiniz")
