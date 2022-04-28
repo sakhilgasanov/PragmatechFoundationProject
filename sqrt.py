9f5ceebed2b2885f135191942ce980e64a0f459d
sakhilgasanov <102470995+sakhilgasanov@users.noreply.github.com>
2022-04-28 22:57:12 +0400

    new code

--- sqrt.py
+++ sqrt.py
@@ -1,4 +1,16 @@
+#from math import sqrt
+#eded=float(input("eded daxil edin: "))
+#kok=sqrt(eded)
+#print(eded, "ededinin koku", "=", kok)
+
+#from math import sqrt
+#x=9
+#sqrt(x)
+#print(sqrt(9.0))
+
 from math import sqrt
-eded=float(input("eded daxil edin: "))
-kok=sqrt(eded)
-print(eded, "ededinin koku", "=", kok)
+x=9
+y=sqrt(x)
+print(y)
+y=2*sqrt(x)-4
+print(y)
