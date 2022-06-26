524c8b2ef57d45ffd53cdd656ece8fed44d1d29c
sakhilgasanov <102470995+sakhilgasanov@users.noreply.github.com>
2022-06-26 13:33:01 +0400

    new code

---
+++ task1.py
@@ -0,0 +1,58 @@
+"""
+soz=input('Bir string daxil edin: ')
+for i in soz:
+    print(i, soz.index(i))
+"""
+
+"""
+eded=int(input('Bir eded daxil edin: '))
+for i in range(0, eded):
+    print(i)
+"""
+
+"""for j in range(12, 0, -1):
+    print(j)
+"""
+
+"""eded=int(input('Ededi daxil edin: '))
+for i in range(1, eded+1):
+    if i%2!=0:
+        print(i)
+"""
+
+"""eded=int(input('Eded daxil edin: '))
+l=[]
+for i in range(1, eded+1):
+    if eded%i==0:
+        l.append(i)
+print(l)
+"""
+
+"""eded=int(input('Eded daxil edin: '))
+bolen_sayi=0
+for i in range(1, eded+1):
+    if eded%i==0:
+        bolen_sayi+=1
+if bolen_sayi<=2:
+    print('Eded sadedir')
+else: 
+    print('Eded murekkebdir')
+"""
+
+    
+"""eded=int(input('Eded daxil edin: '))
+hasil=1
+for i in range(1, eded+1):
+    hasil=hasil*i
+print(hasil)
+"""
+
+"""eded=int(input('Eded daxil edin: '))
+cem=0
+say=0
+for i in range(1, eded+1):
+    cem+=i
+    say+=1
+print(cem/say)
+"""
+
