0c3da889ff5952843948c5a4c728b70232432925
sakhilgasanov <102470995+sakhilgasanov@users.noreply.github.com>
2022-06-27 22:20:08 +0400

    new code

---
+++ exa.py
@@ -0,0 +1,15 @@
+# listdəki elementlərin silinməsi kodu:
+"""l=[1, 2, 3, 4, 5]
+print(l)
+l[:]=[]
+print(l)"""
+
+# daxil edilən sözün ilk hərfinin geri çağırılması: 
+"""x = "Hello"[0]
+print(x)"""
+
+for x in range(10):
+    if x % 2 == 0:
+        continue
+print(x)
+
